a1ec0d98a5784867ca3759f83dbd8138ef3ca6b3
Luis David Villalobos González <villalobosgonzalezluisdavid@gmail.com>
2020-12-17 23:40:13 -0600

    Fix with functions

--- TinyEditor/js/File.js
+++ TinyEditor/js/File.js
@@ -1,26 +1,27 @@
 var fs = require('fs');
 const { exec } = require('child_process');
+var compiled = false;
 
-document.getElementById("button-compiler").onclick = function(event) {
+function compile(){
   fs.writeFile('File/main.cpp', editor.session.getValue(), 'UTF-8', function(err){console.log(err)})
   console.log(editor.session.getValue());
   var command = 'cd File && make'
   exec(command, (err, stdout, stderr) => {
     if(err){
-      console.log(stderr);
-      //console.log(`stderr: ${stderr}`);
-      //console.error(err)
+      //console.log(stderr);
+      console.log(`stderr: ${stderr}`); // this go to output error
+      //console.error(`err: ${err}`);
+      compiled = false;
     }else{
       //console.log(`stderr: ${stderr}`)
       //console.log(`stdout: ${stdout}`);
       //console.log(`stderr: ${stderr}`);
+      compiled = true;
     }
   });
-
 }
 
-
-document.getElementById("button-run").onclick = function(event) {
+function run_program(){
   exec('start File/run.exe', (err, stdout, stderr) => {
     if(err){
       console.log(`stderr: ${stderr}`)
@@ -31,6 +32,17 @@ document.getElementById("button-run").onclick = function(event) {
       //console.log(`stderr: ${stderr}`);
     }
   });
+}
+
+document.getElementById("button-compiler").onclick = function(event) {
+  compile();
+}
+
+
+document.getElementById("button-run").onclick = function(event) {
+  if(compiled){
+    run_program();
+  }
 }
 
 
