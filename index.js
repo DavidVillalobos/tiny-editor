7b0ccbe210a066910290f1eb6c502b63bb9744b0
DavidVillalobos <villalobosgonzalezluisdavid@gmail.com>
2021-02-10 04:06:02 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 /*
 	File: index.js
-	Date: 2021/02/09
-	Author: DavidVillalobos
+	Date: 2021/02/10
+	Author: David Villalobos
 */
+
 // Requirements
 const dirTree = require('directory-tree');
 const http = require('http');
@@ -12,8 +13,9 @@ const colors = require('colors');
 const port = '8080';
 const hostname = '127.0.0.1';
 
-// For generate tree is
-const folder_path = 'C:\\Users\\luisd\\Desktop\\Test';
+// For generate tree is 
+const folder_path = 'C:\\Users\\luisd\\Desktop\\Test'; // <-- Choose a path
+
 let folder_name = folder_path.split("\\")[folder_path.split("\\").length-1]
 const ext = /.*/; // filtered tree
 
@@ -26,10 +28,16 @@ const fill = ' '
 
 // Server
 http.createServer( (request, response) => {
-	console.log(`New request URL : ${request.url}`.white) 
-	response.writeHead(200, { 'Content-Type': 'text/html', });
-	response.write(generateHTML())
-	console.log(`Request Success :D`.green)
+	console.log(`New request URL : ${request.url}`.yellow)
+	if(request.url == '/'){ 
+		response.writeHead(200, { 'Content-Type': 'text/html', });
+		response.write(generateHTML())
+		console.log(`Request Success :D`.green)
+	}else if(request.url == '/css/style.css'){
+		response.writeHead(200, { 'Content-Type': 'text/css', });
+		response.write(fs.readFileSync('css/style.css'))
+		console.log(`Request Success :D`.green)
+	}
 	response.end();  
 }).listen(port, hostname, () => {
 	console.log(`The server was running in http://${hostname}:${port}/`.green);
@@ -38,35 +46,29 @@ http.createServer( (request, response) => {
 
 function printDirectoryTree(myTree, levels) {
 	let result = '';
-	let size = myTree['children'].length - 1;
-	let i = 0;
 	for (let branch of myTree['children']) {
-		/*for (let level = 0; level < levels.length-1; level++) {
-			if (levels[level][1]) {
-				result += line_vertical;
-			} else {
-				result += fill.repeat(2);
+		if(branch['type'] == 'directory'){
+			result += '<button class="button is-white menu-label">' +
+			'<span>' +
+			'<i class="fas fa-angle-right has-text-info"></i>' +
+		 	'</span>' +
+			branch['name'] +
+			'</button>' +
+			'<ul class="menu-list">'
+			if(branch['children']) { // branch has child? (is a directory)
+				let aux_levels = [...levels]
+				aux_levels.push([aux_levels.length-1, true])
+				result += printDirectoryTree(branch, aux_levels)
 			}
-			result += fill;
+			result += '</ul>'
+		}else{
+			result += '<button class="button is-white">' +
+			'<span>' +
+			'	<i class="fas fa-file has-text-info"></i>' +
+			'</span>' +    
+			branch['name'] +
+			'</button>'
 		}
-		if (i < size) {
-			result += start_folder + line_horizontal;
-		} else {
-			result += end_folder + line_horizontal;
-			levels[levels.length-1][1] = false; // close directory in this level
-		}*/
-		result += '<a class="panel-block is-active">'+
-		//'    <span class="panel-icon">'+
-		//'        <i class="fas fa-book" aria-hidden="true"></i>'+
-		//'    </span>'+
-		'    ' + branch['path'].split(folder_path + '\\')[1] + 
-		'</a>'
-		if(branch['children']) { // branch has child?
-			let aux_levels = [...levels]
-			aux_levels.push([aux_levels.length-1, true])
-			result += printDirectoryTree(branch, aux_levels);
-		}
-		i += 1
 	}
 	return result
 }
@@ -74,35 +76,27 @@ function printDirectoryTree(myTree, levels) {
 function generateHTML(){
 	let tree = dirTree(folder_path, { extensions : ext } );
 	if(!tree) return '<h1 style="color: red;">Path not found<h1>'
-	return '<!DOCTYPE html>'+
-	'<html lang="en">'+
-	'<head>'+
-	'	<meta charset="UTF-8">'+
-	'	<meta http-equiv="X-UA-Compatible" content="IE=edge">'+
-	'	<meta name="viewport" content="width=device-width, initial-scale=1.0">'+
-	'	<title>Document</title>'+
-	'	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css">'+
-	'	<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>'+
-	'</head>'+
-	'<body>'+
-	'	<div class="columns">'+
-	'		<div class="column is-2">'+
-	'			<nav class="panel is-warning">'+
-	'				<p class="panel-heading">'+
-	'					' + folder_name + 
-	'				</p>'+
-	'				<div class="panel-block">'+
-	'					<p class="control has-icons-left">'+
-	'						<input class="input" type="text" placeholder="Search">'+
-	'						<span class="icon is-left">'+
-	'							<i class="fas fa-search" aria-hidden="true"></i>'+
-	'						</span>'+
-	'					</p>'+
-	'				</div>'+
-					printDirectoryTree(tree, [[0, true]]) +
-	'			</div>'+
-	'			</nav>'+
-	'		</div>'+
-	'	</body>'+
+	return '<!DOCTYPE html>' +
+	'<html lang="es">' +
+	'<head>' +
+	'	<meta charset="UTF-8">' +
+	'	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css">' +
+	'	<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>' +
+	'	<link rel="stylesheet" href="css/style.css"/>' +
+	'</head>' +
+	'<body>' +
+	'<nav class="panel is-info">' +
+	'	<div class="menu-label panel-heading">' +
+	'		' + folder_name +
+	'	</div>' +
+	'	<div class="panel-block" style=">' +
+	'		<div class="menu">' +
+	'	  	<ul class="menu-list">' +
+	'			<li>' + printDirectoryTree(tree, [[0, true]]) + '</li>' +
+	'	  	</ul>' +
+	'		</div>' +
+	'	</div>' +
+	'</nav>' +
+	'</body>' +
 	'</html>'
 }
