b0dcecde760dc46d61480a83968d5af5adce5f47
DavidVillalobos <villalobosgonzalezluisdavid@gmail.com>
2021-02-28 05:19:27 -0600

    option to pause

---
+++ src/config/pause.cpp
@@ -0,0 +1,15 @@
+/*
+    File: Pause.cpp
+    Author: David Villalobos
+    Date: 28-02-2021
+*/
+
+#include<iostream>
+
+int main(){
+	std::cout << "===================================================\n";
+	std::cout << "The option to pause at the end of the execution is\n";
+	std::cout << "activated, if you want to disable it go to settings\n";
+	std::cout << "Press any key to close window.";
+    std::cin.get();
+}
