51aaea934ed921c7933d658f846dcaeacabae5ff
DavidVillalobos <villalobosgonzalezluisdavid@gmail.com>
2021-03-01 16:27:32 -0600

    update end pause

--- src/config/pause.cpp
+++ src/config/pause.cpp
@@ -7,9 +7,9 @@
 #include<iostream>
 
 int main(){
-	std::cout << "===================================================\n";
-	std::cout << "The option to pause at the end of the execution is\n";
-	std::cout << "activated, if you want to disable it go to settings\n";
-	std::cout << "Press any key to close window.";
+	std::cout << "<<                   Program finished                  >>\n";
+	std::cout << " The option to pause at the end of the execution is\n";
+	std::cout << " activated, if you want to disable it go to settings\n";
+	std::cout << " Press any key to close window.";
     std::cin.get();
 }
